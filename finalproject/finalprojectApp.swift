2f3c88ec654ce18657d4c47e5ba31d865c944471
Spandana Tathicherla <spandy.tathi@gmail.com>
2024-07-26 13:04:13 -0400

    Initial Commit

---
+++ finalproject/finalprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalprojectApp.swift
+//  finalproject
+//
+//  Created by Spandana Tathicherla on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct finalprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
